e089661b0bac1b548c4de414e6529647169b1046
Wilson Brito <wil.brito@icloud.com>
2022-06-13 19:57:38 +0200

    nav en proceso

--- js/index.js
+++ js/index.js
@@ -12,3 +12,14 @@ function myFunction() {
       x.className = "topnav";
     }
   } 
+
+function navResponsiveFix(){
+  let dad = document.getElementById("eleDadAux");
+  let list = dad.children;
+  cont = 1.5;
+  aux = 2.5;
+  for(let i = 1; i<list.length -2; i++){
+      list[i].style.top = aux+cont+"rem";
+      aux += cont; 
+  }
+}
